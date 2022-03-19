70708119dcadc6cdbdcfdd16eecd4bbdf4d47e59
lyzhib <74363315+lyzhib@users.noreply.github.com>
2022-03-19 19:34:00 +0800

    Update Main.java

--- Лю_Юаньчжи/Main.java
+++ Лю_Юаньчжи/Main.java
@@ -4,8 +4,11 @@ public class Main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("Counting DNA Nucleotides:");
+	System.out.println("Counting DNA Nucleotides:");
         CountingDNANucleotides.countNucleotides("resources/rosalind_dna.txt");
+		
+	System.out.println("Transcribing DNA into RNA:");
+        TranscribingDNAintoRNA.transcribeDNA("resources/rosalind_rna.txt");
 	}
 
 }
