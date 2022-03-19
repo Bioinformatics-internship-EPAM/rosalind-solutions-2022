9d057230fc63c47115d04e1aee859b7ea8d23493
lyzhib <74363315+lyzhib@users.noreply.github.com>
2022-03-19 18:52:46 +0800

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,13 @@
+package task;
+
+import task.*;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Counting DNA Nucleotides:");
+        CountingDNANucleotides.countNucleotides("resources/rosalind_dna.txt");
+	}
+
+}
