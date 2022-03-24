dd76c4858356e20eef2d8078eed302df22efe7a3
Ло П <90679190+luopinyao579@users.noreply.github.com>
2022-03-24 11:27:41 +0300

    Create MortalFibonacciRabbits.java

---
+++ src/MortalFibonacciRabbits.java
@@ -0,0 +1,37 @@
+package testofbioinfo;
+
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.List;
+
+public class MortalFibonacciRabbits{
+    public static void main(String[] args) throws IOException {
+        List<String>lines = Files.readAllLines(Paths.get("D:\\project.java\\bioinformatics\\resources\\FIBD.txt"),
+                StandardCharsets.UTF_8);
+        String data = lines.get(0);
+        System.out.println("Mortal Fibonacci Rabbits");
+        System.out.println(data);
+        System.out.println(MortalFR(data));
+    }
+    public static long MortalFR(String database){
+        String[] data = database.split(" ");
+        int n = Integer.parseInt(data[0]);
+        int m = Integer.parseInt(data[1]);
+        List<Long> s =new ArrayList<>();
+        for(int i =0;i<n;i++){
+            if(i<2){
+                s.add(1L);
+            }else if(i<m){
+                s.add(s.get(i-1)+s.get(i-2));
+            }else if(i==m){
+                s.add(s.get(i-1)+s.get(i-2)-1);
+            }else{
+                s.add(s.get(i-1)+s.get(i-2)-s.get(i-m-1));
+            }
+        }
+        return s.get(s.size()-1);
+    }
+}
