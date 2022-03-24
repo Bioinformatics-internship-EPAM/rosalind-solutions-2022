c18187c1fe37fc3418233f2e0fa7b44f1788b571
Ло П <90679190+luopinyao579@users.noreply.github.com>
2022-03-24 11:26:39 +0300

    Create DnaNucleotidesCounter.java

---
+++ src/DnaNucleotidesCounter.java
@@ -0,0 +1,29 @@
+package testofbioinfo;
+
+import java.io.*;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+public class DnaNucleotidesCounter {
+    public static void main(String[] args) throws IOException {
+        List<String> lines = Files.readAllLines(Paths.get("D:\\project.java\\bioinformatics\\resources\\DNA.txt"),
+                StandardCharsets.UTF_8);
+        String dna = lines.get(0);
+        System.out.println("Dna Nucleotides Counter");
+        countFromString(dna);
+    }
+    public static void countFromString(String dna) {
+        if (dna.isEmpty()) return;
+
+        Map<Character, Long> frequencies = dna.chars().boxed().collect(Collectors.groupingBy(
+                c -> (char) c.intValue(), Collectors.counting()));
+        for(char c : new char[]{'A','C','G','T'}){
+            System.out.print(frequencies.getOrDefault(c,0L)+" ");
+        }
+        System.out.println();
+    }
+}
