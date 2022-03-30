39e08bdd6760df8c4b52fe1a62f1f4a28d89d516
Ло П <90679190+luopinyao579@users.noreply.github.com>
2022-03-30 19:19:13 +0300

    Update MortalFibonacciRabbits.java

--- src/MortalFibonacciRabbits.java
+++ src/MortalFibonacciRabbits.java
@@ -8,6 +8,8 @@ import java.util.ArrayList;
 import java.util.List;
 
 //https://rosalind.info/problems/fibd/
+//Given: Positive integers n≤100 and m≤20.
+//Return: The total number of pairs of rabbits that will remain after the n-th month if all rabbits live for m months.
 
 public class MortalFibonacciRabbits{
     public static void main(String[] args) throws IOException {
