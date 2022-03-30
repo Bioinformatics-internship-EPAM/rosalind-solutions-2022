f4b924f2727c5fc5b6df18a0f56a29003a772f32
Ло П <90679190+luopinyao579@users.noreply.github.com>
2022-03-30 19:18:27 +0300

    Update MendelsFirstLaw.java

--- src/MendelsFirstLaw.java
+++ src/MendelsFirstLaw.java
@@ -4,6 +4,9 @@ import java.util.Scanner;
 
 //https://rosalind.info/problems/iprb/
 
+//Given: Three positive integers k, m, and n, representing a population containing k+m+n organisms: k individuals are homozygous dominant for a factor, m are heterozygous, and n are homozygous recessive.
+//Return: The probability that two randomly selected mating organisms will produce an individual possessing a dominant allele (and thus displaying the dominant phenotype). Assume that any two organisms can mate.
+
 public class MendelsFirstLaw {
     public static void main(String[] args) {
         System.out.println("MendelsFristLaw");
