a541cabb41df7901d2a8f158ba8a4727f3120a85
Ло П <90679190+luopinyao579@users.noreply.github.com>
2022-03-30 16:16:51 +0300

    Update MortalFibonacciRabbits.java

--- src/MortalFibonacciRabbits.java
+++ src/MortalFibonacciRabbits.java
@@ -7,6 +7,8 @@ import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.List;
 
+//https://rosalind.info/problems/fibd/
+
 public class MortalFibonacciRabbits{
     public static void main(String[] args) throws IOException {
         List<String>lines = Files.readAllLines(Paths.get("D:\\project.java\\bioinformatics\\resources\\FIBD.txt"),
