ecc647200e618fc54f1fced5a75c23b968f1928d
Ло П <90679190+luopinyao579@users.noreply.github.com>
2022-03-30 19:15:26 +0300

    Update CountingPointMutations.java

--- src/CountingPointMutations.java
+++ src/CountingPointMutations.java
@@ -8,6 +8,9 @@ import java.util.List;
 
 //https://rosalind.info/problems/hamm/
 
+//Given: Two DNA strings s and t of equal length (not exceeding 1 kbp).
+//Return: The Hamming distance dH(s,t).
+
 public class CountingPointMutations {
     public static void main(String[] args) throws IOException {
         List<String> lines = Files.readAllLines(Paths.get("D:\\project.java\\bioinformatics\\resources\\hamm.txt"),
